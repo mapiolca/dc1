548970a4f170fae9b8d7fe6ea070667e271eb967
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-11-26 13:51:49 +0100

    Fix formatting of URL in modDc1.class.php

--- core/modules/modDc1.class.php
+++ core/modules/modDc1.class.php
@@ -57,7 +57,7 @@ class modDc1 extends DolibarrModules
 		$this->description = "Module permettant remplir le formulaire DC1 depuis la proposition commerciale";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 		$this->version = '1.1.3';
-		$this->url_last_version = 'https://moduleversion.lesmetiersdubatiment.fr/ver.php?m=dc1'
+		$this->url_last_version = 'https://moduleversion.lesmetiersdubatiment.fr/ver.php?m=dc1';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
